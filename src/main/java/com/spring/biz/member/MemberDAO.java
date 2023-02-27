3cbd7a82eb40a8673674950c61c5caea4a2486be
seokjeeee <seosg2@naver.com>
2023-02-27 17:09:29 +0900

    Feat. memberDAO, member VO, memberServiceImpl 작성 및 수정

--- src/main/java/com/spring/biz/member/MemberDAO.java
+++ src/main/java/com/spring/biz/member/MemberDAO.java
@@ -5,27 +5,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
-import com.spring.biz.common.JDBCUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+
 
 public class MemberDAO {
 	ArrayList<MemberVO> member;
 	Connection conn;
 	PreparedStatement pstmt;
 
-	final String INSERT = "INSERT INTO MEMBER VALUES(MNUM_SEQ.NEXTVAL, ?, ?, ?, ?, ?, 0, ?, ?, ?, SYSDATE, NULL)";
-	final String INSERT_KAKAO = "INSERT INTO MEMBER VALUES(MNUM_SEQ.NEXTVAL, ?, ?, ?, ?, ?, 0, ?, ?, ?, SYSDATE, ?)";
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	final String INSERT = "INSERT INTO MEMBER (?, ?, ?, ?, ?, 0, ?, ?, ?, SYSDATE, NULL, ?, ?, ?)";
+	final String INSERT_KAKAO = "INSERT INTO MEMBER (?, ?, ?, ?, ?, 0, ?, ?, ?, SYSDATE, ?)";
 	// final String SELECTONE_LOGIN = "SELECT MNUM, MID, MPW, MNAME, MEMAIL, MTEL,
 	// MPOINT, ZIPCODE, USERADDR, DETAILADDR FROM MEMBER WHERE MID=? AND MPW=?";
 	final String SELECTONE_LOGIN = "SELECT MNUM, MID, MNAME FROM MEMBER WHERE MID=? AND MPW=?";
 	final String SELECTONE_KAKAOCHK = "SELECT KAKAOLOGIN FROM MEMBER WHERE MNUM=?";
-	final String SELECTONE_INFO = "SELECT MNUM, MID, MPW, MNAME, MEMAIL, MTEL, MPOINT, ZIPCODE, USERADDR, DETAILADDR FROM MEMBER WHERE MNUM=?";
+	final String SELECTONE_INFO = "SELECT MNUM, MID, MPW, MNAME, MEMAIL, MTEL, MPOINT, ZIPCODE, USERADDR, DETAILADDR, MDATE, SCORE, MSTATUS, MIMG FROM MEMBER WHERE MNUM=?";
 	final String SELECTONE_ID = "SELECT MID FROM MEMBER WHERE MNAME=? AND MEMAIL=?";
 	final String SELECTONE_IDCHK = "SELECT MID FROM MEMBER WHERE MID=?";
 	final String SELECTONE_EMAIL = "SELECT REPLACE(MEMAIL, SUBSTR(MEMAIL,INSTR(MEMAIL, '@', 1, 1)-4, 4 ), '****') AS FINDPW, MEMAIL FROM MEMBER WHERE MID=?";
 	final String SELECTONE_EMAILCHK = "SELECT MEMAIL FROM MEMBER WHERE MEMAIL=?";
 	// 관리자페이지
 	final String SELECTALL = "SELECT TO_CHAR(MDATE, 'DD/DAY') AS TDATE, COUNT(*) AS CNT FROM MEMBER WHERE ROWNUM<=7 GROUP BY TO_CHAR(MDATE, 'DD/DAY') ORDER BY TDATE DESC";
+
 	// 멤버페이지
 	final String SELECTALL_MEMBER = "SELECT * FROM MEMBER ORDER BY MNUM ASC";
 	final String UPDATE_USER = "UPDATE MEMBER SET MPW=?, MEMAIL=?, MTEL=?, ZIPCODE=?, USERADDR=?, DETAILADDR=? WHERE MNUM=?";
@@ -33,263 +41,154 @@ public class MemberDAO {
 	final String UPDATE_PW = "UPDATE MEMBER SET MPW=? WHERE MNUM=?";
 	final String DELETE_USER = "DELETE FROM MEMBER WHERE MNUM=? AND MPW=?";
 	final String DELETE_ADMIN = "DELETE FROM MEMBER WHERE MNUM=?";
+	//멤버 상태
+	final String UPDATE_STATUS = "UPDATE MEMBER SET MSTATUS=? WHERE MNUM=?";
+	//멤버 포인트
+	final String UPDATE_POINT = "UPDATE MEMBER SET MPOINT=? WHERE MNUM=?";
+	//멤버 점수
+	final String UPDATE_SCORE = "UPDATE MEMBER SET SCORE=? WHERE MNUM=?";
+	//매칭디테일
+	final String SELECTALL_MATCHING = "SELECT M.MNUM, M.MNAME, M.SCORE,B.BRATE, M.MIMG FROM MEMBER M, BOARD B, APPLICANT A WHERE M.MNUM = B.MNUM and A.MNUM = B.MNUM and B.BNUM=? ORDER BY M.MNUM asc";
+	
 
-	public boolean insert(MemberVO mvo) {
-		conn = JDBCUtil.connect();
-		int cnt = 0;
-		try {
-			if (mvo.getKakao() != null) {
-				pstmt = conn.prepareStatement(INSERT_KAKAO);
-				cnt++;
-			} else {
-				pstmt = conn.prepareStatement(INSERT);
-			}
 
-			pstmt.setString(1, mvo.getmId());
-			pstmt.setString(2, mvo.getmPw());
-			pstmt.setString(3, mvo.getmName());
-			pstmt.setString(4, mvo.getmEmail());
-			pstmt.setString(5, mvo.getmTel());
-			pstmt.setString(6, mvo.getZipCode());
-			pstmt.setString(7, mvo.getUserAddr());
-			pstmt.setString(8, mvo.getDetailAddr());
-			if (cnt == 1) {
-				pstmt.setString(9, mvo.getKakao());
-			}
 
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
+	public boolean insertMember(MemberVO vo) {
+		jdbcTemplate.update(INSERT,vo.getmId(),vo.getmPw(),vo.getmName(),vo.getmEmail(),vo.getmTel(),vo.getmPoint(),vo.getZipCode(),vo.getUserAddr(),vo.getDetailAddr(),vo.getScore(),vo.getMstatus(),vo.getmImg());
+		return true;
+	}
+	
+	public boolean updatePw(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_PW, vo.getmPw(),vo.getmNum());
+		if(res<1) {
 			return false;
-		} finally {
-			JDBCUtil.disconnect(conn, pstmt);
 		}
 		return true;
 	}
-
-	public ArrayList<MemberVO> selectAll(MemberVO mvo) {
-		ArrayList<MemberVO> datas = new ArrayList<MemberVO>();
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTALL);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				MemberVO data = new MemberVO();
-				data.setTempDate(rs.getString("TDATE"));
-				data.setTempCnt(rs.getInt("CNT"));
-				datas.add(data);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+	public boolean updateUser(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_USER, vo.getmPw(),vo.getmEmail(),vo.getmTel(),vo.getZipCode(),vo.getUserAddr(),vo.getDetailAddr(),vo.getmNum());
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return datas;
+		return true;
 	}
-
-	public ArrayList<MemberVO> selectAllMember(MemberVO mvo) {
-		ArrayList<MemberVO> datas = new ArrayList<MemberVO>();
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTALL_MEMBER);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				MemberVO data = new MemberVO();
-				data.setmNum(rs.getInt("MNUM"));
-				data.setmId(rs.getString("MID"));
-				data.setmPw(rs.getString("MPW"));
-				data.setmName(rs.getString("MNAME"));
-				data.setmEmail(rs.getString("MEMAIL"));
-				data.setmTel(rs.getString("MTEL"));
-				data.setmPoint(rs.getInt("MPOINT"));
-				data.setZipCode(rs.getString("ZIPCODE"));
-				data.setUserAddr(rs.getString("USERADDR"));
-				data.setDetailAddr(rs.getString("DETAILADDR"));
-				data.setmDate(rs.getDate("MDATE"));
-				data.setKakao(rs.getString("KAKAOLOGIN"));
-				datas.add(data);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+	public boolean updateAdmin(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_ADMIN, vo.getmPw(),vo.getmName(),vo.getmEmail(),vo.getmTel(),vo.getZipCode(),vo.getUserAddr(),vo.getDetailAddr(),vo.getmPoint(),vo.getmNum());
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return datas;
+		return true;
 	}
-
-	public MemberVO selectOneLogin(MemberVO mvo) {
-		MemberVO data = null;
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTONE_LOGIN);
-			pstmt.setString(1, mvo.getmId());
-			pstmt.setString(2, mvo.getmPw());
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				data = new MemberVO();
-				data.setmNum(rs.getInt("MNUM"));
-				data.setmId(rs.getString("MID"));
-				data.setmName(rs.getString("MNAME"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+	
+	public boolean updateStatus(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_STATUS, vo.getMstatus(),vo.getmNum());
+		
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return data;
-	}
-
-	public MemberVO selectOneInfo(MemberVO mvo) {
-		MemberVO data = null;
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTONE_INFO);
-			pstmt.setInt(1, mvo.getmNum());
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				data = new MemberVO();
-				data.setmNum(rs.getInt("MNUM"));
-				data.setmId(rs.getString("MID"));
-				data.setmPw(rs.getString("MPW"));
-				data.setmName(rs.getString("MNAME"));
-				data.setmEmail(rs.getString("MEMAIL"));
-				data.setmTel(rs.getString("MTEL"));
-				data.setmPoint(rs.getInt("MPOINT"));
-				data.setZipCode(rs.getString("ZIPCODE"));
-				data.setUserAddr(rs.getString("USERADDR"));
-				data.setDetailAddr(rs.getString("DETAILADDR"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		return true;
+}
+	
+	
+	public boolean updateScore(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_SCORE, vo.getScore(),vo.getmNum());
+		
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return data;
-	}
-
-	public MemberVO selectOneId(MemberVO mvo) {
-		MemberVO data = null;
-		conn = JDBCUtil.connect();
-		try {
-			if (mvo.getmEmail() == null) {
-				pstmt = conn.prepareStatement(SELECTONE_IDCHK);
-				pstmt.setString(1, mvo.getmId());
-			} else {
-				pstmt = conn.prepareStatement(SELECTONE_ID);
-				pstmt.setString(1, mvo.getmName());
-				pstmt.setString(2, mvo.getmEmail());
-			}
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				data = new MemberVO();
-				data.setmId(rs.getString("MID"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		return true;
+}
+	
+	public boolean updatePoint(MemberVO vo) {
+		int res=jdbcTemplate.update(UPDATE_POINT, vo.getmPoint(),vo.getmNum());
+		
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return data;
-	}
-
-	public MemberVO selectOneEmailCHK(MemberVO mvo) {
-		MemberVO data = null;
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTONE_EMAILCHK);
-			pstmt.setString(1, mvo.getmEmail());
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				data = new MemberVO();
-				data.setmEmail(rs.getString("MEMAIL"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		return true;
+}
+	
+	public boolean deleteAdmin(MemberVO vo) {
+		int res=jdbcTemplate.update(DELETE_ADMIN, vo.getmNum());
+		if(res<1) {
+			return false;
 		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return data;
+		return true;
 	}
-
-	public MemberVO selectOneFindPw(MemberVO mvo) {
-		MemberVO data = null;
-		conn = JDBCUtil.connect();
-		try {
-			pstmt = conn.prepareStatement(SELECTONE_EMAIL);
-			pstmt.setString(1, mvo.getmId());
-
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				data = new MemberVO();
-				data.setmEmail(rs.getString("MEMAIL"));
-				data.setFindPw(rs.getString("FINDPW"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return data;
-	}
-
-	public boolean update(MemberVO mvo) {
-		conn = JDBCUtil.connect();
-		try {
-			if (mvo.getmTel() == null) {
-				pstmt = conn.prepareStatement(UPDATE_PW);
-				pstmt.setString(1, mvo.getmPw());
-				pstmt.setInt(2, mvo.getmNum());
-			} else if (mvo.getmName() == null) {
-				pstmt = conn.prepareStatement(UPDATE_USER);
-				pstmt.setString(1, mvo.getmPw());
-				pstmt.setString(2, mvo.getmEmail());
-				pstmt.setString(3, mvo.getmTel());
-				pstmt.setString(4, mvo.getZipCode());
-				pstmt.setString(5, mvo.getUserAddr());
-				pstmt.setString(6, mvo.getDetailAddr());
-				pstmt.setInt(7, mvo.getmNum());
-			} else {
-				pstmt = conn.prepareStatement(UPDATE_ADMIN);
-				pstmt.setString(1, mvo.getmPw());
-				pstmt.setString(2, mvo.getmName());
-				pstmt.setString(3, mvo.getmEmail());
-				pstmt.setString(4, mvo.getmTel());
-				pstmt.setString(5, mvo.getZipCode());
-				pstmt.setString(6, mvo.getUserAddr());
-				pstmt.setString(7, mvo.getDetailAddr());
-				pstmt.setInt(8, mvo.getmPoint());
-				pstmt.setInt(9, mvo.getmNum());
-			}
-
-			int res = pstmt.executeUpdate();
-			if (res <= 0) {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+	
+	public boolean deleteUser(MemberVO vo) {
+		int res=jdbcTemplate.update(DELETE_USER, vo.getmNum(),vo.getmPw());
+		if(res<1) {
 			return false;
 		}
 		return true;
 	}
 
-	public boolean delete(MemberVO mvo) {
-		conn = JDBCUtil.connect();
-		try {
-			if (mvo.getmPw() == null) {
-				pstmt = conn.prepareStatement(DELETE_ADMIN);
-				pstmt.setInt(1, mvo.getmNum());
-			} else {
-				pstmt = conn.prepareStatement(DELETE_USER);
-				pstmt.setInt(1, mvo.getmNum());
-				pstmt.setString(2, mvo.getmPw());
-			}
-
-			int res = pstmt.executeUpdate();
-			if (res <= 0) {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-		JDBCUtil.disconnect(conn, pstmt);
-		return true;
+	public List<MemberVO> selectAll(MemberVO vo) {
+		return jdbcTemplate.query(SELECTALL, new MemberRowMapper());
 	}
+	
+	public List<MemberVO> selectAllMember(MemberVO vo) {
+		return jdbcTemplate.query(SELECTALL_MEMBER, new MemberRowMapper());
+	}
+	
+	
+	public MemberVO selectOneLogin(MemberVO vo) {
+		Object[] args= { vo.getmId(),vo.getmPw() };
+		return jdbcTemplate.queryForObject(SELECTONE_LOGIN, args, new MemberRowMapper());
+	}
+	
+	public MemberVO selectOneInfo(MemberVO vo) {
+		Object[] args= { vo.getmNum() };
+		return jdbcTemplate.queryForObject(SELECTONE_INFO, args, new MemberRowMapper());
+	}
+	
+	public MemberVO selectOneId(MemberVO vo) {
+		Object[] args= {vo.getmName(), vo.getmEmail() };
+		return jdbcTemplate.queryForObject(SELECTONE_ID, args, new MemberRowMapper());
+	}
+	
+	public MemberVO selectOneEmailCHK(MemberVO vo) {
+		Object[] args= {vo.getmId()};
+		return jdbcTemplate.queryForObject(SELECTONE_IDCHK, args, new MemberRowMapper());
+	}
+	
+	public MemberVO selectOneFindPw(MemberVO vo) {
+		Object[] args= {vo.getmId()};
+		return jdbcTemplate.queryForObject(SELECTONE_EMAIL, args, new MemberRowMapper());
+	}
+	
+	
+	public List<MemberVO> selectAllMatching(MemberVO vo) {
+		return jdbcTemplate.query(SELECTALL, new MemberRowMapper());
+		
+	}
+	
+	
+
+	class MemberRowMapper implements RowMapper<MemberVO> {
+
+		@Override
+		public MemberVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			MemberVO data=new MemberVO();
+			data.setmNum(rs.getInt("MNUM"));
+			data.setmId(rs.getString("MID"));
+			data.setmPw(rs.getString("MPW"));
+			data.setmName(rs.getString("MNAME"));
+			data.setmEmail(rs.getString("MEMAIL"));
+			data.setmTel(rs.getString("MTEL"));
+			data.setmPoint(rs.getInt("MPOINT"));
+			data.setZipCode(rs.getString("ZIPCODE"));
+			data.setUserAddr(rs.getString("USERADDR"));
+			data.setDetailAddr(rs.getString("DETAILADDR"));
+			data.setScore(rs.getInt("SCORE"));
+			data.setMstatus(rs.getString("DELETE_ADMIN"));
+			data.setmImg(rs.getString("DELETE_ADMIN"));
+			return data;
+		}
+		
+	}
+
+
 
 }
--- src/main/java/com/spring/biz/member/MemberServiceImpl.java
+++ src/main/java/com/spring/biz/member/MemberServiceImpl.java
@@ -12,52 +12,52 @@ public class MemberServiceImpl implements MemberService {
 	private MemberDAO memberDAO;
 
 	@Override
-	public boolean insert(MemberVO mvo) {
-		return memberDAO.insert(mvo);
+	public boolean insert(MemberVO vo) {
+		return memberDAO.insertMember(vo);
 	}
 
 	@Override
-	public List<MemberVO> selectAll(MemberVO mvo) {
-		return memberDAO.selectAll(mvo);
+	public List<MemberVO> selectAll(MemberVO vo) {
+		return memberDAO.selectAllMember(vo);
 	}
 
 	@Override
-	public List<MemberVO> selectAllMember(MemberVO mvo) {
-		return memberDAO.selectAllMember(mvo);
+	public List<MemberVO> selectAllMember(MemberVO vo) {
+		return memberDAO.selectAllMember(vo);
 	}
 
 	@Override
-	public MemberVO selectOneLogin(MemberVO mvo) {
-		return memberDAO.selectOneLogin(mvo);
+	public MemberVO selectOneLogin(MemberVO vo) {
+		return memberDAO.selectOneLogin(vo);
 	}
 
 	@Override
-	public MemberVO selectOneInfo(MemberVO mvo) {
-		return memberDAO.selectOneInfo(mvo);
+	public MemberVO selectOneInfo(MemberVO vo) {
+		return memberDAO.selectOneInfo(vo);
 	}
 
 	@Override
-	public MemberVO selectOneId(MemberVO mvo) {
-		return memberDAO.selectOneId(mvo);
+	public MemberVO selectOneId(MemberVO vo) {
+		return memberDAO.selectOneId(vo);
 	}
 
 	@Override
-	public MemberVO selectOneEmailCHK(MemberVO mvo) {
-		return memberDAO.selectOneEmailCHK(mvo);
+	public MemberVO selectOneEmailCHK(MemberVO vo) {
+		return memberDAO.selectOneEmailCHK(vo);
 	}
 
 	@Override
-	public MemberVO selectOneFindPw(MemberVO mvo) {
-		return memberDAO.selectOneFindPw(mvo);
+	public MemberVO selectOneFindPw(MemberVO vo) {
+		return memberDAO.selectOneFindPw(vo);
 	}
 
 	@Override
-	public boolean update(MemberVO mvo) {
-		return memberDAO.update(mvo);
+	public boolean update(MemberVO vo) {
+		return memberDAO.updateUser(vo);
 	}
 
 	@Override
-	public boolean delete(MemberVO mvo) {
-		return memberDAO.delete(mvo);
+	public boolean delete(MemberVO vo) {
+		return memberDAO.deleteUser(vo);
 	}
 }
