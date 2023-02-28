d996c3919c82354c66f963e20f163529cf6297ba
polodumbo <polosh1208@gmail.com>
2023-02-28 09:58:04 +0900

    Refactor: SweetAlert 관련 수정

---
+++ src/main/java/com/spring/biz/common/SweetAlertDTO.java
@@ -0,0 +1,32 @@
+package com.spring.biz.common;
+
+public class SweetAlertDTO {
+	private String title;
+	private String msg;
+	private String url;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
