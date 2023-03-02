7824afe13564f5882d8a9de0f10a53c629ae7ed1
Subin Jo <subiiin6085@naver.com>
2023-03-02 15:34:14 +0900

    Refactor & Fix : ProductDAO JdbcTemplate 적용 및 SQL문 (product 관련 select문 / 필터검색 ) 수정

--- src/main/java/com/spring/biz/product/ProductDAO.java
+++ src/main/java/com/spring/biz/product/ProductDAO.java
@@ -1,13 +1,12 @@
 package com.spring.biz.product;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository("productDAO")
@@ -19,14 +18,14 @@ public class ProductDAO {
 	// C : Product
 	final String INSERT = "INSERT INTO PRODUCT (CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, INFOIMG, PRODUCTCNT) VALUES(?, ?, ?, ?, SYSDATE(), ?, ?, ?, ?, ?, ?)";
 	// R : Product
-	final String SELECTONE = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, INFOIMG, PRODUCTCNT FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A WHERE A.PNUM=?";
+	final String SELECTONE = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, INFOIMG, PRODUCTCNT FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A WHERE A.PNUM=?";
 	final String SELECTONE_CART = "SELECT PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, BRAND, PIMG FROM PRODUCT WHERE PNUM=?";
-	final String SELECTALL_NEW = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A ORDER BY A.RDATE DESC) B LIMIT ?";
-	final String SELECTALL_BEST = "SELECT * FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.PNUM, P.CATENUM, P.PNAME, P.FIXPRICE, P.SELPRICE, P.RDATE, P.REPERSON, P.REAGE, P.BRAND, P.PIMG, P.PRODUCTCNT, CASE WHEN O.CNT IS NULL THEN 0 ELSE O.CNT END OCNT FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM RIGHT OUTER JOIN (SELECT PNUM, SUM(CNT) CNT FROM ORDERDETAIL GROUP BY PNUM) O ON P.PNUM=O.PNUM ORDER BY OCNT DESC) A LIMIT ?";
-	final String SELECTALL_CATEALL = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A WHERE CATENUM BETWEEN ? AND ? ORDER BY RDATE DESC) B LIMIT ?";
-	final String SELECTALL_CATE = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A WHERE CATENUM=? ORDER BY RDATE DESC) B LIMIT ?";
-	final String SELECTALL_AGE = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, CASE WHEN REAGE BETWEEN 0 AND 3 THEN 1 WHEN REAGE BETWEEN 4 AND 6 THEN 2 WHEN REAGE BETWEEN 7 AND 10 THEN 3 WHEN REAGE BETWEEN 11 AND 13 THEN 4 WHEN REAGE >= 14 THEN 5 END AS CATEAGE FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A LIMIT ?";
-	final String SELECTALL = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, INFOIMG, PRODUCTCNT FROM (SELECT CASE D.MNUM WHEN ? THEN 1 ELSE 0 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM) A";
+	final String SELECTALL_NEW = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A ORDER BY A.RDATE DESC) B LIMIT ?";
+	final String SELECTALL_BEST = "SELECT * FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.PNUM, P.CATENUM, P.PNAME, P.FIXPRICE, P.SELPRICE, P.RDATE, P.REPERSON, P.REAGE, P.BRAND, P.PIMG, P.PRODUCTCNT, CASE WHEN O.CNT IS NULL THEN 0 ELSE O.CNT END OCNT FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM LEFT OUTER JOIN (SELECT PNUM, SUM(CNT) CNT FROM ORDERDETAIL GROUP BY PNUM) O ON P.PNUM=O.PNUM ORDER BY OCNT DESC) A LIMIT ?";
+	final String SELECTALL_CATEALL = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A WHERE CATENUM BETWEEN ? AND ? ORDER BY RDATE DESC) B LIMIT ?";
+	final String SELECTALL_CATE = "SELECT * FROM (SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A WHERE CATENUM=? ORDER BY RDATE DESC) B LIMIT ?";
+	final String SELECTALL_AGE = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, CASE WHEN REAGE BETWEEN 0 AND 3 THEN 1 WHEN REAGE BETWEEN 4 AND 6 THEN 2 WHEN REAGE BETWEEN 7 AND 10 THEN 3 WHEN REAGE BETWEEN 11 AND 13 THEN 4 WHEN REAGE >= 14 THEN 5 END AS CATEAGE FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A LIMIT ?";
+	final String SELECTALL = "SELECT DIB, PNUM, CATENUM, PNAME, FIXPRICE, SELPRICE, RDATE, REPERSON, REAGE, BRAND, PIMG, INFOIMG, PRODUCTCNT FROM (SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.* FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM) A";
 	// R : Category
 	final String SELECTONE_CATE = "SELECT TRUNCATE(CATENUM, -2) AS CATENUM, CATEL, CATEM FROM CATEGORY WHERE CATENUM=?";
 	final String SELECTALL_LCATE = "SELECT DISTINCT CATEL, CASE MOD(CATENUM, 100) WHEN 0 THEN CATENUM ELSE CATENUM-MOD(CATENUM, 100) END CATENUM FROM CATEGORY WHERE CATENUM BETWEEN ? AND ?";
@@ -39,8 +38,7 @@ public class ProductDAO {
 
 	public boolean insert(ProductVO pvo) {
 		try {
-			template.update(INSERT, pvo.getCateNum(), pvo.getpName(), pvo.getFixPrice(), pvo.getSelPrice(), pvo.getRePerson(), pvo.getReAge(), pvo.getBrand(), pvo.getpImg(), pvo.getInfoImg(),
-					pvo.getProductCnt());
+			template.update(INSERT, pvo.getCateNum(), pvo.getpName(), pvo.getFixPrice(), pvo.getSelPrice(), pvo.getRePerson(), pvo.getReAge(), pvo.getBrand(), pvo.getpImg(), pvo.getInfoImg(), pvo.getProductCnt());
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
@@ -49,100 +47,140 @@ public class ProductDAO {
 	}
 
 	public CategoryVO selectOneCate(ProductVO pvo) {
-		Object[] args = { pvo.getCateNum() };
-		return template.queryForObject(SELECTONE_CATE, args, new CategoryRowMapper());
+		try {
+			return template.queryForObject(SELECTONE_CATE, BeanPropertyRowMapper.newInstance(CategoryVO.class), pvo.getCateNum());
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public ProductVO selectOne(ProductVO pvo) {
 		Object[] args = { pvo.getDib(), pvo.getpNum() };
-		return template.queryForObject(SELECTONE, args, new ProductRowMapper());
+		return template.queryForObject(SELECTONE, args, BeanPropertyRowMapper.newInstance(ProductVO.class));
 	}
 
 	public ProductVO selectOneCart(ProductVO pvo) {
-		Object[] args = { pvo.getpNum() };
-		return template.queryForObject(SELECTONE, args, new ProductRowMapper());
+		try {
+			return template.queryForObject(SELECTONE, BeanPropertyRowMapper.newInstance(ProductVO.class), pvo.getpNum());
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public List<ProductVO> selectAll(ProductVO pvo) {
-		if (pvo.getCateNum() == 1000) {
-			Object[] args = { pvo.getDib(), pvo.getPageCnt() };
-			return template.query(SELECTALL_NEW, args, new ProductRowMapper());
-		} else if (pvo.getCateNum() == 1100) {
-			Object[] args = { pvo.getDib(), pvo.getPageCnt() };
-			return template.query(SELECTALL_BEST, args, new ProductRowMapper());
-		} else if (pvo.getCateNum() == 100) {
-			Object[] args = { pvo.getDib() };
-			return template.query(SELECTALL, args, new ProductRowMapper());
-		} else if (pvo.getCateNum() > 100 && pvo.getCateNum() < 200) {
-			Object[] args = { pvo.getDib(), pvo.getPageCnt() };
-			return template.query(SELECTALL_AGE, args, new ProductRowMapper());
-		} else if (pvo.getCateNum() % 100 == 0) {
-			Object[] args = { pvo.getDib(), pvo.getLowNum(), pvo.getHighNum() + 99, pvo.getPageCnt() };
-			return template.query(SELECTALL_CATEALL, args, new ProductRowMapper());
-		} else {
-			Object[] args = { pvo.getDib(), pvo.getCateNum(), pvo.getPageCnt() };
-			return template.query(SELECTALL_CATE, args, new ProductRowMapper());
+		Object[] args;
+		String query;
+		List<ProductVO> datas = new ArrayList<>();
+		try {
+			if (pvo.getCateNum() == 1000) {
+				args = new Object[] { pvo.getDib(), pvo.getPageCnt() };
+				query = SELECTALL_NEW;
+			} else if (pvo.getCateNum() == 1100) {
+				args = new Object[] { pvo.getDib(), pvo.getPageCnt() };
+				query = SELECTALL_BEST;
+			} else if (pvo.getCateNum() == 100) {
+				args = new Object[] { pvo.getDib() };
+				query = SELECTALL;
+			} else if (pvo.getCateNum() > 100 && pvo.getCateNum() < 200) {
+				args = new Object[] { pvo.getDib(), pvo.getPageCnt() };
+				query = SELECTALL_AGE;
+			} else if (pvo.getCateNum() % 100 == 0) {
+				args = new Object[] { pvo.getDib(), pvo.getLowNum(), pvo.getHighNum() + 99, pvo.getPageCnt() };
+				query = SELECTALL_CATEALL;
+			} else {
+				args = new Object[] { pvo.getDib(), pvo.getCateNum(), pvo.getPageCnt() };
+				query = SELECTALL_CATE;
+			}
+			datas = template.query(query, args, BeanPropertyRowMapper.newInstance(ProductVO.class));
 		}
+		catch(EmptyResultDataAccessException e) {
+			e.printStackTrace();
+		}
+		return datas;
 	}
 
 	public List<CategoryVO> selectAll(CategoryVO cvo) {
-		if (cvo.getLowNum() != 0) {
-			Object[] args = { cvo.getLowNum(), cvo.getHighNum() };
-			return template.query(SELECTALL_LCATE, args, new CategoryRowMapper());
-		} else {
-			Object[] args = { cvo.getCateL() };
-			return template.query(SELECTALL_MCATENAME, args, new CategoryRowMapper());
+		Object[] args;
+		String query;
+		List<CategoryVO> datas = new ArrayList<>();
+		try {
+			if (cvo.getLowNum() != 0) {
+				args = new Object[] { cvo.getLowNum(), cvo.getHighNum() };
+				query = SELECTALL_LCATE;
+			} else {
+				args = new Object[] { cvo.getCateL() };
+				query = SELECTALL_MCATENAME;
+			}
+			datas = template.query(query, args, BeanPropertyRowMapper.newInstance(CategoryVO.class));
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
 		}
+		return datas;
 	}
 
 	public List<CategoryVO> selectAllCateNum(CategoryVO cvo) {
 		Object[] args = { cvo.getCateNum(), cvo.getCateNum() };
-		return template.query(SELECTALL_MCATENUM, args, new CategoryRowMapper());
+		List<CategoryVO> datas = new ArrayList<>();
+		try {
+			datas = template.query(SELECTALL_MCATENUM, args, BeanPropertyRowMapper.newInstance(CategoryVO.class));
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+		}
+		return datas;
 	}
 
 	public List<ProductVO> selectAllSearch(ProductVO pvo) {
-		ProductVO data0 = template.queryForObject("SELECT MAX(SELPRICE) MAXPRICE FROM PRODUCT", new ProductRowMapper());
+		ProductVO data0 = template.queryForObject("SELECT MAX(SELPRICE) HIGHNUM FROM PRODUCT", BeanPropertyRowMapper.newInstance(ProductVO.class));
 		String searchBar = "SELECT * FROM PRODUCT WHERE SELPRICE BETWEEN ? AND ?";
-
-		if (pvo.getCateNum() != 0) {
-			searchBar += " AND CATENUM=? ";
-		}
-		if (pvo.getpName() != null) {
-			searchBar += " AND PNAME LIKE CONCAT('%', ?, '%') ";
-		}
-		if (pvo.getBrand() != null) {
-			searchBar += " AND Brand=? ";
-		}
-
-		System.out.println("SearhBar : " + searchBar);
-
 		ArrayList<Object> arList = new ArrayList<Object>();
+		List<ProductVO> datas = new ArrayList<>();
+		
 		arList.add(pvo.getLowNum());
 		arList.add(pvo.getHighNum() == 0 ? data0.getHighNum() : pvo.getHighNum());
 
 		if (pvo.getCateNum() != 0) {
+			searchBar += " AND CATENUM=? ";
 			arList.add(pvo.getCateNum());
 		}
 		if (pvo.getpName() != null) {
+			searchBar += " AND PNAME LIKE CONCAT('%', ?, '%') ";
 			arList.add(pvo.getpName());
 		}
 		if (pvo.getBrand() != null) {
+			searchBar += " AND BRAND=? ";
 			arList.add(pvo.getBrand());
 		}
 
-		Object[] args1 = arList.toArray();
-		return template.query(searchBar, args1, new ProductRowMapper());
+		System.out.println("\t[ ProductDAO 로그 ]SearhBar SQL문: " + searchBar);
+		Object[] args = arList.toArray();
+		
+		try {
+			datas = template.query(searchBar, args, BeanPropertyRowMapper.newInstance(ProductVO.class));
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+		}
+		
+		return datas;
 	}
 
 	public List<ProductVO> selectAllFilter(ProductVO pvo) {
-		String filter = "SELECT CASE D.MNUM WHEN 1 THEN 1 ELSE 0 END DIB, P.PNUM, P.CATENUM, P.PNAME, P.FIXPRICE, P.SELPRICE, P.RDATE, P.REPERSON, P.REAGE, P.BRAND, P.PIMG, P.PRODUCTCNT, CASE WHEN O.CNT IS NULL THEN 0 ELSE O.CNT END OCNT FROM PRODUCT P LEFT OUTER JOIN DIB D ON P.PNUM = D.PNUM RIGHT OUTER JOIN (SELECT PNUM, SUM(CNT) CNT FROM ORDERDETAIL GROUP BY PNUM) O ON P.PNUM=O.PNUM WHERE 1=1";
-
+		String filter = "SELECT CASE WHEN D.MNUM IS NULL THEN 0 ELSE 1 END DIB, P.PNUM, P.CATENUM, P.PNAME, P.FIXPRICE, P.SELPRICE, P.RDATE, P.REPERSON, P.REAGE, P.BRAND, P.PIMG, P.PRODUCTCNT, CASE WHEN O.CNT IS NULL THEN 0 ELSE O.CNT END OCNT FROM PRODUCT P LEFT OUTER JOIN (SELECT PNUM, MNUM FROM DIB WHERE MNUM=?) D ON P.PNUM = D.PNUM LEFT OUTER JOIN (SELECT PNUM, SUM(CNT) CNT FROM ORDERDETAIL GROUP BY PNUM) O ON P.PNUM=O.PNUM WHERE 1=1";
+		ArrayList<Object> arList = new ArrayList<Object>();
+		List<ProductVO> datas = new ArrayList<>();
+		
+		arList.add(pvo.getDib());
+		
 		if ((pvo.getCateNum() > 99 && pvo.getCateNum() < 300) || (pvo.getCateNum() > 999 && pvo.getCateNum() < 1200)) {
 			filter += " AND 1=1 ";
 		} else if (pvo.getCateNum() % 100 == 0) {
 			filter += " AND CATENUM BETWEEN ? AND ?";
+			arList.add(pvo.getCateNum());
+			arList.add(pvo.getCateNum() + 99);
 		} else {
 			filter += " AND CATENUM=?";
+			arList.add(pvo.getCateNum());
 		}
 
 		if (pvo.getFilterTags() == 31) {
@@ -165,6 +203,7 @@ public class ProductDAO {
 
 		if (pvo.getpName() != null) {
 			filter += " AND PNAME LIKE CONCAT('%', ?, '%') ";
+			arList.add(pvo.getpName());
 		}
 
 		if (pvo.getFilterSortBy() == 11) {
@@ -177,42 +216,39 @@ public class ProductDAO {
 			filter += " ORDER BY SELPRICE DESC";
 		}
 
-		ArrayList<Object> arList = new ArrayList<Object>();
-
-		arList.add(pvo.getDib());
-
-		if ((pvo.getCateNum() > 99 && pvo.getCateNum() < 300) || (pvo.getCateNum() > 999 && pvo.getCateNum() < 1200)) {
-
-		} else if (pvo.getCateNum() % 100 == 0) {
-			arList.add(pvo.getCateNum());
-			arList.add(pvo.getCateNum() + 99);
-		} else {
-			arList.add(pvo.getCateNum());
-		}
-
-		if (pvo.getpName() != null) {
-			arList.add(pvo.getpName());
-		}
-
 		Object[] args = arList.toArray();
-		return template.query(filter, args, new ProductRowMapper());
+		try {
+			datas = template.query(filter, args, BeanPropertyRowMapper.newInstance(ProductVO.class));
+		} catch (EmptyResultDataAccessException e) {
+			e.printStackTrace();
+		}
+		return datas;
 	}
 
 	public boolean update(ProductVO pvo) {
-		template.update(UPDATE, pvo.getCateNum(), pvo.getpName(), pvo.getFixPrice(), pvo.getSelPrice(), pvo.getReAge(), pvo.getBrand(), pvo.getProductCnt(), pvo.getpNum());
+		try {
+			template.update(UPDATE, pvo.getCateNum(), pvo.getpName(), pvo.getFixPrice(), pvo.getSelPrice(), pvo.getReAge(), pvo.getBrand(), pvo.getProductCnt(), pvo.getpNum());
+		} catch(Exception e) {
+			return false;
+		}
 		return true;
 	}
 
 	public boolean delete(ProductVO pvo) {
-		template.update(DELETE, pvo.getpNum());
+		try {
+			template.update(DELETE, pvo.getpNum());
+		} catch(Exception e) {
+			return false;
+		}
 		return true;
 	}
 }
 
-class ProductRowMapper implements RowMapper<ProductVO> {
+/*class ProductRowMapper implements RowMapper<ProductVO> {
 	@Override
 	public ProductVO mapRow(ResultSet rs, int rowNum) throws SQLException {
 		ProductVO data = new ProductVO();
+		data.setDib(rs.getInt("DIB"));
 		data.setpNum(rs.getInt("PNUM"));
 		data.setCateNum(rs.getInt("CATENUM"));
 		data.setpName(rs.getString("PNAME"));
@@ -240,4 +276,4 @@ class CategoryRowMapper implements RowMapper<CategoryVO> {
 		data.setHighNum(rs.getInt("HIGHNUM"));
 		return data;
 	}
-}
+}*/
