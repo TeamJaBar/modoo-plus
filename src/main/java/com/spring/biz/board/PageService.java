9bb290b237885f04cb847d0fd8b1fc3486cfd41f
minsu <kun7347@naver.com>
2023-03-05 18:44:11 +0900

    Feat : 페이징 관련 DAO, sql, controller 수정

--- src/main/java/com/spring/biz/board/PageService.java
+++ src/main/java/com/spring/biz/board/PageService.java
@@ -3,10 +3,15 @@ package com.spring.biz.board;
 import java.util.List;
 
 import com.spring.biz.member.MemberVO;
+import com.spring.biz.sue.SueVO;
 
 public interface PageService {
-	public int getTotal();
+	public int getMainTotal();
 	public int getSueTotal();
 	public int getMyTotal(MemberVO mvo);
-	public List<BoardVO> getList(int pageNum, int amount);
+	public int getMyMatchTotal(MemberVO mvo);
+	public List<BoardVO> getMainList(int pageNum, int amount);
+	public List<BoardVO> getMyList(int pageNum, int amount);
+	public List<SueVO> getSueList(int pageNum, int amount);
+	public List<BoardVO> getMyMatchList(int pageNum, int amount);
 }
--- src/main/java/com/spring/controller/PageController.java
+++ src/main/java/com/spring/controller/PageController.java
@@ -13,6 +13,7 @@ import com.spring.biz.board.BoardVO;
 import com.spring.biz.board.PageService;
 import com.spring.biz.board.PageVO;
 import com.spring.biz.member.MemberVO;
+import com.spring.biz.sue.SueVO;
 
 @Controller
 @RequestMapping(value = "/view")
@@ -20,7 +21,8 @@ public class PageController {
 
 	@Autowired
 	PageService pageService;
-	//마이페이지로 이동
+
+	// 마이페이지로 이동
 	@RequestMapping(value = "/mypage.do")
 	public String myPaging(HttpServletRequest request, Model model, PageVO vo, MemberVO mvo) {
 		// 1. 화면전환 시에 조회하는 페이지번호 and 화면에 그려질 데이터개수 2개를 전달받음
@@ -36,28 +38,27 @@ public class PageController {
 
 		try {
 			mvo.setmNum(Integer.parseInt(request.getParameter("mNum")));
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return "error.jsp";
 		}
-		
-		// 2. pageVO생성
-		List<BoardVO> list = pageService.getList(pageNum, amount);
-		int total =pageService.getMyTotal(mvo); // 전체게시글수
-		PageVO pageVO = new PageVO(pageNum, amount, total);
 
+		// 2. pageVO생성
+		List<BoardVO> list = pageService.getMyList(pageNum, amount);
+		int total = pageService.getMyTotal(mvo); // 전체게시글수
+		PageVO pageVO = new PageVO(pageNum, amount, total);
 
 		// 3. 페이지네이션을 화면에 전달
 		model.addAttribute("pageVO", pageVO);
 		// 화면에 가지고 나갈 list를 request에 저장 !!
 		model.addAttribute("list", list);
-		
+
 		return "mypage.jsp";
 	}
 	
-	// 신고글 목록 페이지로 이동시
-	@RequestMapping(value = "/adMoveSue.do")
-	public String suePaging(HttpServletRequest request, Model model, PageVO vo) {
+	// 마이매치페이지로 이동
+	@RequestMapping(value = "/myBoard.do")
+	public String myMatchPaging(HttpServletRequest request, Model model, PageVO vo, MemberVO mvo) {
 		// 1. 화면전환 시에 조회하는 페이지번호 and 화면에 그려질 데이터개수 2개를 전달받음
 		// 첫 페이지 경우
 		int pageNum = 1;
@@ -68,18 +69,78 @@ public class PageController {
 			pageNum = Integer.parseInt(request.getParameter("pageNum"));
 			amount = Integer.parseInt(request.getParameter("amount"));
 		}
-		
+
+		try {
+			mvo.setmNum(Integer.parseInt(request.getParameter("mNum")));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error.jsp";
+		}
+
 		// 2. pageVO생성
-		List<BoardVO> list = pageService.getList(pageNum, amount);
-		int total = pageService.getSueTotal(); // 전체게시글수
+		List<BoardVO> list = pageService.getMyList(pageNum, amount);
+		int total = pageService.getMyTotal(mvo); // 전체게시글수
 		PageVO pageVO = new PageVO(pageNum, amount, total);
 
+		// 3. 페이지네이션을 화면에 전달
+		model.addAttribute("pageVO", pageVO);
+		// 화면에 가지고 나갈 list를 request에 저장 !!
+		model.addAttribute("list", list);
+
+		return "mymatch.jsp";
+	}
+
+	// 신고글 목록 페이지로 이동시
+	@RequestMapping(value = "/adMoveSue.do")
+	public String suePaging(HttpServletRequest request, Model model, PageVO vo) {
+		// 1. 화면전환 시에 조회하는 페이지번호 and 화면에 그려질 데이터개수 2개를 전달받음
+		// 첫 페이지 경우
+		System.out.println("신고글 페이징 컨트롤러 진입");
+		int pageNum = 1;
+		int amount = 10;
+
+		// 페이지번호를 클릭하는 경우
+		if (request.getParameter("pageNum") != null && request.getParameter("amount") != null) {
+			pageNum = Integer.parseInt(request.getParameter("pageNum"));
+			amount = Integer.parseInt(request.getParameter("amount"));
+		}
+
+		// 2. SueVO생성
+		List<SueVO> list = pageService.getSueList(pageNum, amount);
+		int total = pageService.getSueTotal(); // 전체게시글수
+		PageVO pageVO = new PageVO(pageNum, amount, total);
 
 		// 3. 페이지네이션을 화면에 전달
 		model.addAttribute("pageVO", pageVO);
 		// 화면에 가지고 나갈 list를 request에 저장 !!
 		model.addAttribute("list", list);
 		return "admin-sue.jsp";
+	}
+
+	// 메인페이지로 이동시
+	@RequestMapping(value = "/main.do")
+	public String mainPaging(HttpServletRequest request, Model model, PageVO vo) {
+		// 1. 화면전환 시에 조회하는 페이지번호 and 화면에 그려질 데이터개수 2개를 전달받음
+		// 첫 페이지 경우
+		int pageNum = 1;
+		int amount = 10;
+
+		// 페이지번호를 클릭하는 경우
+		if (request.getParameter("pageNum") != null && request.getParameter("amount") != null) {
+			pageNum = Integer.parseInt(request.getParameter("pageNum"));
+			amount = Integer.parseInt(request.getParameter("amount"));
+		}
+
+		// 2. pageVO생성
+		List<BoardVO> list = pageService.getMainList(pageNum, amount);
+		int total = pageService.getMainTotal(); // 전체게시글수
+		PageVO pageVO = new PageVO(pageNum, amount, total);
+
+		// 3. 페이지네이션을 화면에 전달
+		model.addAttribute("pageVO", pageVO);
+		// 화면에 가지고 나갈 list를 request에 저장 !!
+		model.addAttribute("list", list);
+		return "main.jsp";
 	}
 
 }
