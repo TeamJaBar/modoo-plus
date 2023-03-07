a22e1332f45f29670747b4a0ccdf7bb64b6d0c41
kyeon1 <rkdussla444@naver.com>
2023-03-07 17:24:26 +0900

    Feat:"Mybatis 적용한 CommentDAO 수정";

--- src/main/java/com/spring/biz/board/CommentDAO.java
+++ src/main/java/com/spring/biz/board/CommentDAO.java
@@ -1,27 +1,29 @@
 package com.spring.biz.board;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
-@Repository("commentDAO")
+//@Repository("commentDAO")
 public class CommentDAO {
 	@Autowired
-	private SqlSessionTemplate mybatis;
+	private JdbcTemplate jdbcTemplate;
 	//매칭 댓글 INSERT,UPDATE,DELETE,SELECTALL
-//	private final String SQL_INSERT="INSERT INTO COMMENT (MNUM, CCONTENT, CWDATE) VALUES(?, ?, SYSDATE())";
-//	//INSERT 한거 보여주는 SELECTALL
-//	private final String SQL_UPDATE="UPDATE COMMENT SET CCONTENT=?, CCDATE=SYSDATE()  WHERE CNUM=?";
-//	private final String SQL_DELETE="DELETE FROM COMMENT WHERE CNUM=?";
-//	//전체 댓글 출력(수정)
-//	private final String SQL_SELECTALL="SELECT C.CNUM, CCONTENT, CWDATE, MID, CCDATE FROM COMMENT C JOIN MEMBER M ON C.MNUM = M.MNUM JOIN BOARD B ON B.BNUM=C.BNUM WHERE B.BNUM= ? ORDER BY CWDATE ASC";
+	private final String SQL_INSERT="INSERT INTO COMMENT (MNUM, CCONTENT, CWDATE) VALUES(?, ?, SYSDATE())";
+	//INSERT 한거 보여주는 SELECTALL
+	private final String SQL_UPDATE="UPDATE COMMENT SET CCONTENT=?, CCDATE=SYSDATE()  WHERE CNUM=?";
+	private final String SQL_DELETE="DELETE FROM COMMENT WHERE CNUM=?";
+	//전체 댓글 출력(수정)
+	private final String SQL_SELECTALL="SELECT C.CNUM, CCONTENT, CWDATE, MID, CCDATE FROM COMMENT C JOIN MEMBER M ON C.MNUM = M.MNUM JOIN BOARD B ON B.BNUM=C.BNUM WHERE B.BNUM= ? ORDER BY CWDATE ASC";
 
 	public boolean insertComment(CommentVO cvo) {
 		try {
 			System.out.println("CommentDAO의 insert()");
-			mybatis.insert("CommentDAO.insertComment",cvo);
+			jdbcTemplate.update(SQL_INSERT,cvo.getmNum(),cvo.getcContent(),cvo.getcWdate());
 		}catch(Exception e) {
 			return false;
 		}
@@ -31,7 +33,7 @@ public class CommentDAO {
 	public boolean updateComment(CommentVO cvo) {
 		try {
 			System.out.println("CommentDAO의 update()");
-			mybatis.update("CommentDAO.updateComment",cvo);
+			jdbcTemplate.update(SQL_UPDATE, cvo.getcContent(),cvo.getcCdate(),cvo.getcNum());
 		}catch(Exception e) {
 			return false;
 		}
@@ -41,7 +43,7 @@ public class CommentDAO {
 	public boolean deleteComment(CommentVO cvo) {
 		try {
 			System.out.println("CommentDAO의 delete()");
-			mybatis.delete("CommentDAO.deleteComment",cvo);
+			jdbcTemplate.update(SQL_DELETE,cvo.getcNum());
 		}catch(Exception e) {
 			return false;
 		}
@@ -50,25 +52,26 @@ public class CommentDAO {
 	
 	public List<CommentVO> selectAll(CommentVO cvo){
 		try {
-			return mybatis.selectList("CommentDAO.selectAll",cvo);
+			return jdbcTemplate.query(SQL_SELECTALL, new CommentRowMapper(),cvo.getcCdate());
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-//	class CommentRowMapper implements RowMapper<CommentVO>{
-//
-//		@Override
-//		public CommentVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-//			CommentVO datas=new CommentVO();
-//			datas.setcContent(rs.getString("CCOMMENT"));
-//			datas.setcWdate(rs.getTimestamp("CWDATE"));
-//			datas.setcWdate(rs.getTimestamp("CWDATE"));
-//			datas.setmId(rs.getString("MID"));
-//			datas.setcCdate(rs.getTimestamp("CCDATE"));
-//			datas.setcNum(rs.getInt("CNUM"));
-//			return datas;
-//		}
-//	}
+	class CommentRowMapper implements RowMapper<CommentVO>{
+
+		@Override
+		public CommentVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			CommentVO datas=new CommentVO();
+			datas.setcContent(rs.getString("CCOMMENT"));
+			datas.setcWdate(rs.getTimestamp("CWDATE"));
+			datas.setcWdate(rs.getTimestamp("CWDATE"));
+			datas.setmId(rs.getString("MID"));
+			datas.setcCdate(rs.getTimestamp("CCDATE"));
+			datas.setcNum(rs.getInt("CNUM"));
+			return datas;
+		}
+
+	}
 }
