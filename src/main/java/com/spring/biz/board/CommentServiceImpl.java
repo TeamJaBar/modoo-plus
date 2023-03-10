4ce8b90a2121555ad5e131744c36710a88a7c377
seokjeeee <seosg2@naver.com>
2023-03-10 10:28:06 +0900

    Feat: CommentVO, CommentDAO, CommentServiceImpl 수정

--- src/main/java/com/spring/biz/board/CommentServiceImpl.java
+++ src/main/java/com/spring/biz/board/CommentServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class CommentServiceImpl implements CommentService{
 
 	@Autowired
-	CommentDAO commentDAO;
+	CommentDAO2 commentDAO;
 	
 	@Override
 	public boolean insertComment(CommentVO cvo) {
