54b8206c71633095a65c3054a87c5b5d0d2f45d2
Subin Jo <subiiin6085@naver.com>
2023-03-13 10:44:49 +0900

    Feat : 경로에 폴더 없을시 폴더를 생성하는 makeFolder 메소드 추가

--- src/main/java/com/spring/biz/common/ImageUploadUtil.java
+++ src/main/java/com/spring/biz/common/ImageUploadUtil.java
@@ -9,8 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ImageUploadUtil {
 	
-	private static final String PROFILE_PATH= "ModooPlus/src/main/webapp/assets/img/avatar/";
-	private static final String BOARD_PATH="ModooPlus/src/main/webapp/assets/img/board/";
+	private static final String PROFILE_PATH= "ModooPlus/src/main/webapp/assets/img/avatar";
+	private static final String BOARD_PATH="ModooPlus/src/main/webapp/assets/img/board";
 	
 	public static String getImgFileName(HttpServletRequest request, MultipartFile uploadFile, String type) {
 		String path = request.getSession().getServletContext().getRealPath("/");
@@ -21,15 +21,40 @@ public class ImageUploadUtil {
 		
 		try {
 			if(type.equals("profile")) {
-				uploadFile.transferTo(new File(path+PROFILE_PATH+fileName));
+				makeFolder(type);
+				uploadFile.transferTo(new File(path+PROFILE_PATH+"/"+fileName));
 			} else {
-				uploadFile.transferTo(new File(path+BOARD_PATH+fileName));
+				makeFolder(type);
+				uploadFile.transferTo(new File(path+BOARD_PATH+"/"+fileName));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}	
 		
 		return fileName;
+	}
+	
+	private static void makeFolder(String type) {
+		File folder;
+		if(type.equals("profile")) {
+			folder = new File(PROFILE_PATH);
+		} else {
+			 folder = new File(BOARD_PATH);
+		}
+
+		// 해당 디렉토리가 없다면 디렉토리를 생성.
+		if (!folder.exists()) {
+			try{
+			    folder.mkdir(); //폴더 생성
+			    System.out.println("폴더가 생성완료.");
+		        } 
+		        catch(Exception e){
+			    e.getStackTrace();
+			}        
+	         }else {
+			System.out.println("폴더가 이미 존재합니다..");
+		}
 	}
 	
 	private static String uuidFileName(String originalFileName) {
