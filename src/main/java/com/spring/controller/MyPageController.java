7e397c7a984c200ad8f7b9d7e6fe5a832481c221
polodumbo <polosh1208@gmail.com>
2023-03-13 14:51:13 +0900

    Style: MyPageController 정렬

--- src/main/java/com/spring/controller/MyPageController.java
+++ src/main/java/com/spring/controller/MyPageController.java
@@ -28,9 +28,9 @@ import com.spring.biz.member.MemberService;
 import com.spring.biz.member.MemberVO;
 
 @Controller
-@RequestMapping(value="/view")
+@RequestMapping(value = "/view")
 public class MyPageController {
-	
+
 	@Autowired
 	BoardService boardService;
 	@Autowired
@@ -41,11 +41,10 @@ public class MyPageController {
 	PageService pageService;
 	@Autowired
 	CommentService commentService;
-	
+
 	private static final int DEFAULT_PAGENUM = 1;
 	private static final int DEFAULT_AMOUNT = 10;
-	
-	
+
 	@RequestMapping("/mypage.do")
 	public String selectAllMyBoard(BoardVO bvo, PageVO pvo, HttpSession session, Model model) {
 		bvo.setAmount(DEFAULT_AMOUNT);
@@ -54,10 +53,10 @@ public class MyPageController {
 			bvo.setPageNum(DEFAULT_PAGENUM);
 		}
 
-		if(session.getAttribute("mNum") != null) {
-			bvo.setmNum((Integer)session.getAttribute("mNum"));			
+		if (session.getAttribute("mNum") != null) {
+			bvo.setmNum((Integer)session.getAttribute("mNum"));
 		}
-		
+
 		int total = pageService.getMyTotal(bvo); // 전체게시글수
 		pvo = new PageVO(bvo.getPageNum(), total);
 
@@ -66,47 +65,45 @@ public class MyPageController {
 		model.addAttribute("bDatas", pageService.selectAllMyBoard(bvo));
 		return "/view/plus/mypage.jsp";
 	}
-	
 
-	@RequestMapping(value= "/updateBoardAction.do", method=RequestMethod.GET)
+	@RequestMapping(value = "/updateBoardAction.do", method = RequestMethod.GET)
 	public String updateBAction(BoardVO bvo, HttpServletRequest request) {
-		//보드 매칭상태 업데이트 (모집중 OR 모집완료)
+		// 보드 매칭상태 업데이트 (모집중 OR 모집완료)
 		String referer = request.getHeader("Referer");
 		System.out.println(bvo);
 		boardService.updateBoard(bvo);
-		return "redirect:"+referer;
+		return "redirect:" + referer;
 	}
-	
 
-	@RequestMapping(value = "/updateBoardAction.do", method=RequestMethod.POST)
+	@RequestMapping(value = "/updateBoardAction.do", method = RequestMethod.POST)
 	public @ResponseBody String updateBActionAjax(BoardVO bvo) {
-		if(boardService.updateBoard(bvo)) {
+		if (boardService.updateBoard(bvo)) {
 			return "1";
 		} else {
 			return "-1";
 		}
 	}
-	
-	@RequestMapping(value="/boardDelete.do", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/boardDelete.do", method = RequestMethod.GET)
 	public String deleteBoard(BoardVO bvo, HttpServletRequest request) {
-		//내가 작성한 글 삭제
+		// 내가 작성한 글 삭제
 		String referer = request.getHeader("Referer");
 		boardService.deleteBoard(bvo);
-		if(referer.contains("boardDetail")) {
+		if (referer.contains("boardDetail")) {
 			return "redirect:boardList.do?sortBy=1";
 		}
-		return "redirect:"+referer;
+		return "redirect:" + referer;
 	}
-	
-	@RequestMapping(value = "/boardDelete.do", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/boardDelete.do", method = RequestMethod.POST)
 	public @ResponseBody String deleteOneBoard(BoardVO bvo) {
-		if(boardService.deleteBoard(bvo)) {
+		if (boardService.deleteBoard(bvo)) {
 			return "1";
 		} else {
 			return "-1";
 		}
 	}
-	
+
 	@RequestMapping("/myBoard.do")
 	public String selectAllMyMatch(BoardVO bvo, PageVO pvo, HttpSession session, Model model) {
 		bvo.setAmount(DEFAULT_AMOUNT);
@@ -115,10 +112,10 @@ public class MyPageController {
 			bvo.setPageNum(DEFAULT_PAGENUM);
 		}
 
-		if(session.getAttribute("mNum") != null) {
-			bvo.setmNum((Integer)session.getAttribute("mNum"));			
+		if (session.getAttribute("mNum") != null) {
+			bvo.setmNum((Integer)session.getAttribute("mNum"));
 		}
-		
+
 		int total = pageService.getMyMatchTotal(bvo); // 전체게시글수
 		pvo = new PageVO(bvo.getPageNum(), total);
 		String calendar = new Gson().toJson(boardService.selectAllManage(bvo));
@@ -129,36 +126,35 @@ public class MyPageController {
 		model.addAttribute("calendar", calendar);
 		return "/view/plus/mymatch.jsp";
 	}
-	
-	//내가 신청한 매칭 목록 - 매칭 신청 취소
-	@RequestMapping(value="/myEntryDelete.do", method=RequestMethod.GET)
+
+	// 내가 신청한 매칭 목록 - 매칭 신청 취소
+	@RequestMapping(value = "/myEntryDelete.do", method = RequestMethod.GET)
 	public String deleteMatch(ApplicantVO avo, HttpServletRequest request) {
 		String referer = request.getHeader("Referer");
 		applicantService.delete(avo);
-		return "redirect:"+referer;
-	}
-	
-	//내가 신청한 매칭 목록 - 매칭 신청 취소
-	@RequestMapping(value="/myEntryDelete.do", method=RequestMethod.POST)
-	public @ResponseBody String deleteMatchAjax(ApplicantVO avo) {
-		if(applicantService.delete(avo)) {
-			return "1";
-		}
-		return "-1";
-		
+		return "redirect:" + referer;
 	}
 
-	
-	//내가 작성한 글 - 매칭 강퇴
+	// 내가 신청한 매칭 목록 - 매칭 신청 취소
+	@RequestMapping(value = "/myEntryDelete.do", method = RequestMethod.POST)
+	public @ResponseBody String deleteMatchAjax(ApplicantVO avo) {
+		if (applicantService.delete(avo)) {
+			return "1";
+		}
+		return "-1";
+
+	}
+
+	// 내가 작성한 글 - 매칭 강퇴
 	@RequestMapping("/kickOut.do")
 	public @ResponseBody String deleteApplicant(ApplicantVO avo, HttpServletRequest request, Model model) {
-		if(applicantService.delete(avo)) {
+		if (applicantService.delete(avo)) {
 			return "1";
 		}
 		return "-1";
 	}
-	
-	//평가하기
+
+	// 평가하기
 	@RequestMapping("/userRating.do")
 	public String updateScore(MemberVO mvo, ApplicantVO avo, HttpServletRequest request) {
 		String referer = request.getHeader("Referer");
@@ -166,58 +162,57 @@ public class MyPageController {
 		mvo.setScore(mvp);
 		memberService.update(mvo);
 		applicantService.update(avo);
-		return "redirect:"+referer;
+		return "redirect:" + referer;
 	}
-	
-	//검색
-	@RequestMapping(value={"/searchBoard.do", "/sortBoard.do"})
+
+	// 검색
+	@RequestMapping(value = { "/searchBoard.do", "/sortBoard.do" })
 	public @ResponseBody List<BoardVO> updateScore(BoardVO bvo, Model model) {
 		return boardService.selectAllMain(bvo);
 	}
 
-	
-	//보드 디테일
+	// 보드 디테일
 	@RequestMapping("/boardDetail.do")
 	public String selectOneBoard(BoardVO bvo, ApplicantVO avo, HttpSession session, CommentVO cvo, Model model) {
-		if(session.getAttribute("mNum") != null) {
+		if (session.getAttribute("mNum") != null) {
 			bvo.setmNum((Integer)session.getAttribute("mNum"));
 		}
 		model.addAttribute("bDatas", boardService.selectOne(bvo));
 		model.addAttribute("aDatas", applicantService.selectAll(avo));
 		model.addAttribute("cDatas", commentService.selectAll(cvo));
-		//신고 카테고리
-		//신고 여부
+		// 신고 카테고리
+		// 신고 여부
 		return "/view/plus/match-detail.jsp";
 	}
-	
+
 	@RequestMapping("/insertApplicant.do")
 	public String insertApplicant(ApplicantVO avo, HttpServletRequest request) {
 		String referer = request.getHeader("Referer");
 		avo.setmNum((Integer)request.getSession().getAttribute("mNum"));
 		applicantService.insert(avo);
-		return "redirect:"+referer;
+		return "redirect:" + referer;
 	}
-	
-	@RequestMapping(value="/updateBoard.do", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/updateBoard.do", method = RequestMethod.GET)
 	public String updateView(BoardVO bvo, Model model, HttpServletRequest request) {
 		request.getSession().setAttribute("updateReferer", request.getHeader("Referer"));
 		model.addAttribute("bDatas", boardService.selectOne(bvo));
 		return "/view/plus/match-insert.jsp";
 	}
-	
-	@RequestMapping(value = "/updateBoard.do", method=RequestMethod.POST)
-	public String updateBoard(BoardVO bvo, HttpSession session, @RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm") Date bDate) {
+
+	@RequestMapping(value = "/updateBoard.do", method = RequestMethod.POST)
+	public String updateBoard(BoardVO bvo, HttpSession session, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") Date bDate) {
 		bvo.setbLocal(boardService.getbLocal(bvo.getbAddress()));
 		bvo.setbDate(bDate);
 		String referer = (String)session.getAttribute("updateReferer");
-		if(boardService.updateBoard(bvo)) {
+		if (boardService.updateBoard(bvo)) {
 			session.removeAttribute("updateReferer");
 			System.out.println(referer);
 		}
-		return "redirect:"+referer;
+		return "redirect:" + referer;
 	}
-	
-	@RequestMapping(value = "/showApplicant.do", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/showApplicant.do", method = RequestMethod.POST)
 	public @ResponseBody List<ApplicantVO> selectAllApplicant(ApplicantVO avo, HttpSession session) {
 		avo.setmNum((Integer)session.getAttribute("mNum"));
 		return applicantService.selectAll(avo);
