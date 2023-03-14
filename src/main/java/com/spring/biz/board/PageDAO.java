6cb539c2b17577168a9f678500cd72d392110d59
Subin Jo <subiiin6085@naver.com>
2023-03-14 11:32:01 +0900

    Fix : PageDAO selectAllSue 수정

--- src/main/java/com/spring/biz/board/PageDAO.java
+++ src/main/java/com/spring/biz/board/PageDAO.java
@@ -26,8 +26,7 @@ public class PageDAO {
 	// 마이 페이지 내가 신청한 매칭 목록
 	private final String SQL_SELECTALL_MYMATCH = "SELECT * FROM (SELECT B.BNUM, COUNT(*) ACNT FROM BOARD B LEFT JOIN APPLICANT A ON B.BNUM=A.BNUM GROUP BY B.BNUM) C, (SELECT A.ANUM, A.ACHK, B.BNUM, BTITLE, BADDRESS, BDATE, BCNT, BACTION FROM BOARD B, APPLICANT A WHERE B.BNUM=A.BNUM AND A.MNUM=?) D WHERE C.BNUM = D.BNUM ORDER BY C.BNUM ASC LIMIT ?,?";
 	// 관리자 페이지 신고글 목록
-	private final String SQL_SELECTALL_SUE = "SELECT S.SNUM, S.MNUM, B.BNUM, B.BTITLE, M.MID, M.MIMG, B.BWDATE, B.BSTATUS FROM MEMBER M, BOARD B, SUE S WHERE M.MNUM = B.MNUM AND B.BNUM = S.BNUM ORDER BY B.BNUM ASC LIMIT ?,?";
-	
+	private final String SQL_SELECTALL_SUE = "SELECT S.SNUM, B.BTITLE, M.MID, M.MIMG, S.SDATE, S.SRESULT FROM MEMBER M, BOARD B, SUE S WHERE M.MNUM = B.MNUM AND B.BNUM = S.BNUM ORDER BY S.SNUM ASC LIMIT ?,?";
 	// 총 신고글이 몇개인지
 	public int getSueTotal(SueVO svo) {
 		System.out.println("PageDAO : getSueTotal 실행");
@@ -94,13 +93,11 @@ public class PageDAO {
 				datas= jdbcTemplate.query(SQL_SELECTALL_SUE, (rs, rowNum)->{
 					SueVO tmpData= new SueVO();
 					tmpData.setsNum(rs.getInt("SNUM"));
-					tmpData.setmNum(rs.getInt("MNUM"));
-					tmpData.setbNum(rs.getInt("BNUM"));
 					tmpData.setbTitle(rs.getString("BTITLE"));
 					tmpData.setmId(rs.getString("MID"));
 					tmpData.setmImg(rs.getString("MIMG"));
-					tmpData.setbWdate(rs.getTimestamp("BWDATE"));
-					tmpData.setbStatus(rs.getString("BSTATUS"));
+					tmpData.setsDate(rs.getTimestamp("SDATE"));
+					tmpData.setsResult(rs.getString("SRESULT"));
 					return tmpData;
 				}, (svo.getPageNum() - 1) * svo.getAmount(), svo.getAmount());
 		}catch(Exception e) {
