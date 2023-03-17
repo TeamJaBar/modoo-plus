2651b25d1ae161bc1c17854fa733f2f527d47176
minsu <kun7347@naver.com>
2023-03-17 15:34:08 +0900

    Feat : EmailSender Repository로 변경

--- src/main/java/com/spring/biz/common/EmailSender.java
+++ src/main/java/com/spring/biz/common/EmailSender.java
@@ -7,7 +7,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Repository;
 
+@Repository("emailSender")
 public class EmailSender {
 	
     @Autowired
