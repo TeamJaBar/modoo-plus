bfc0d03a4f429601bfe0a37b0689b86d14972b38
polodumbo <polosh1208@gmail.com>
2023-03-21 13:53:45 +0900

    Style: ApplicantVO 코드 정렬

--- src/main/java/com/spring/biz/board/ApplicantVO.java
+++ src/main/java/com/spring/biz/board/ApplicantVO.java
@@ -1,67 +1,83 @@
 package com.spring.biz.board;
 
 public class ApplicantVO {
-	private int aNum; //PK
-	private int bNum; 
+	private int aNum; // PK
+	private int bNum;
 	private int mNum;
 	private String aChk;
-	//JAVA에서만 저장용으로 사용되는 데이터
-	private String mId; //아이디
-	private String mName; //이름
-	private String mImg; //프로필 사진
-	private int score; //점수
-	
+	// JAVA에서만 저장용으로 사용되는 데이터
+	private String mId; // 아이디
+	private String mName; // 이름
+	private String mImg; // 프로필 사진
+	private int score; // 점수
+
 	public int getaNum() {
 		return aNum;
 	}
+
 	public int getbNum() {
 		return bNum;
 	}
+
 	public void setbNum(int bNum) {
 		this.bNum = bNum;
 	}
+
 	public int getmNum() {
 		return mNum;
 	}
+
 	public void setmNum(int mNum) {
 		this.mNum = mNum;
 	}
+
 	public String getaChk() {
 		return aChk;
 	}
+
 	public void setaChk(String aChk) {
 		this.aChk = aChk;
 	}
+
 	public String getmId() {
 		return mId;
 	}
+
 	public void setmId(String mId) {
 		this.mId = mId;
 	}
+
 	public String getmName() {
 		return mName;
 	}
+
 	public void setmName(String mName) {
 		this.mName = mName;
 	}
+
 	public String getmImg() {
 		return mImg;
 	}
+
 	public void setmImg(String mImg) {
 		this.mImg = mImg;
 	}
+
 	public void setaNum(int aNum) {
 		this.aNum = aNum;
 	}
+
 	public int getScore() {
 		return score;
 	}
+
 	public void setScore(int score) {
 		this.score = score;
 	}
 
-
-	
-	
+	@Override
+	public String toString() {
+		return "ApplicantVO [aNum=" + aNum + ", bNum=" + bNum + ", mNum=" + mNum + ", aChk=" + aChk + ", mId=" + mId + ", mName=" + mName + ", mImg=" + mImg + ", score=" + score + "]";
 	}
 
+}
